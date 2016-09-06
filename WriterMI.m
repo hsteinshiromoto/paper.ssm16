c6f09ab0eb6d8735e40bfd95fc191df53cd1ff58
humberto.shiromoto <humberto.shiromoto@ieee.org>
2016-09-06 16:36:28 +1000

    Remove the Option variable; Change the MI formulation

--- WriterMI.m
+++ WriterMI.m
@@ -9,35 +9,14 @@
 % 
 % function WriterMI1(SystemStates,LaplacianMatrix,ScalingFactor)
 
-function WriterMI(SystemStates,LaplacianMatrix,ScalingFactor,lambda,Option)
+function WriterMI(SystemStates,LaplacianMatrix,ScalingFactor,lambda)
 
 fid = fopen('PreProcessedMI.m','w');
 
 NumberOfAgents = size(LaplacianMatrix,1);
 NumberOfStates = length(SystemStates);
 
-% for AgentsCounter = 1:NumberOfAgents
-%     
-%     for NeighborCounter = 1:NumberOfAgents
-%             
-%             if LaplacianMatrix(AgentsCounter,NeighborCounter) ~= 0
-%                 
-%                 fprintf(fid, 'Component%s%s = A%s%s*W%s + transpose(A%s%s*W%s) + ',num2str(AgentsCounter),num2str(NeighborCounter),num2str(AgentsCounter),num2str(NeighborCounter),num2str(NeighborCounter),num2str(NeighborCounter),num2str(AgentsCounter),num2str(AgentsCounter));
-%                 fprintf(fid, 'B%s*Y%s%s + transpose(B%s*Y%s%s);\n',num2str(AgentsCounter),num2str(AgentsCounter),num2str(NeighborCounter),num2str(NeighborCounter),num2str(NeighborCounter),num2str(AgentsCounter));
-%                 
-%             else
-%                 
-%                 fprintf(fid, 'Component%s%s = %d;\n',num2str(AgentsCounter),num2str(NeighborCounter),0);
-%                 
-%             end
-%         
-%     end
-%     fprintf(fid, '\n');
-% end
-
-fprintf(fid, 'MI = -DW + A*W + transpose(A*W) + B*Y + transpose(B*Y) + 2*%f*W;\n\n',lambda);
-
-if strcmp(Option,'Diagonal') || strcmp(Option,'Neighbor')
+fprintf(fid, 'MI = -DW + A*W + transpose(A*W) + R*B*transpose(B) + 2*%f*W;\n\n',lambda);
     
     for AgentsCounter = 1:NumberOfAgents - 1
         fprintf(fid, 'Block%s = ',num2str(AgentsCounter));
@@ -52,11 +31,7 @@ if strcmp(Option,'Diagonal') || strcmp(Option,'Neighbor')
     end
     fprintf(fid, '];');
     
-else
-    
-    fprintf(fid, 'MIConstraints = sos(-MI + %f*eye(size(MI)));\n',ScalingFactor);
-    
-end
+
 
 fclose(fid);
 

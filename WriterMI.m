e2f7caa2e4ec95f8657187f0ae4b30bb43c5be71
humberto.shiromoto <humberto.shiromoto@ieee.org>
2016-09-07 09:30:11 +1000

    Modify the WriterMI. Now much less general.

--- WriterMI.m
+++ WriterMI.m
@@ -1,16 +1,16 @@
-function test
+% function test
+% 
+% SystemStates = ['x','y','z'];
+% ScalingFactor = 1e-2;
+% lambda = 2;
+% LaplacianMatrix = LinearLaplacianGenerator(3)
+% WriterMI1(SystemStates,LaplacianMatrix,ScalingFactor,lambda)
+% 
+% end
+% 
+% function WriterMI1(SystemStates,LaplacianMatrix,ScalingFactor,lambda)
 
-SystemStates = ['x','y','z'];
-ScalingFactor = 1e-2;
-lambda = 2;
-LaplacianMatrix = LinearLaplacianGenerator(3)
-WriterMI1(SystemStates,LaplacianMatrix,ScalingFactor,lambda)
-
-end
-
-function WriterMI1(SystemStates,LaplacianMatrix,ScalingFactor,lambda)
-
-% function WriterMI(SystemStates,LaplacianMatrix,ScalingFactor,lambda)
+function WriterMI(SystemStates,LaplacianMatrix,ScalingFactor,lambda)
 
 fid = fopen('PreProcessedMI.m','w');
 
@@ -19,9 +19,16 @@ NumberOfStates = length(SystemStates);
 
 fprintf(fid, 'MI = -DW + A*W + transpose(A*W) + R*B*transpose(B) + 2*%f*W;\n\n',lambda);
     
-    for AgentsCounter = 1:NumberOfAgents - 1
+    for AgentsCounter = 1:NumberOfAgents
         fprintf(fid, 'Block%s = ',num2str(AgentsCounter));
-        fprintf(fid, 'MI(%s:%s,%s:%s);\n',num2str(NumberOfStates*AgentsCounter - 2),num2str(2*NumberOfStates*AgentsCounter - 2*(AgentsCounter-2)),num2str(NumberOfStates*AgentsCounter - 1),num2str(2*NumberOfStates*AgentsCounter - 2*(AgentsCounter-1)));
+        
+        if AgentsCounter == 1
+            fprintf(fid, 'MI(1:3,1:3);\n');
+        elseif AgentsCounter == 2
+            fprintf(fid, 'MI(4:6,4:6);\n');
+        else
+            fprintf(fid, 'MI(7:9,7:9);\n');
+        end
         fprintf(fid, 'Block%s(3,3) = Block%s(3,3)/2;\n',num2str(AgentsCounter),num2str(AgentsCounter));
         fprintf(fid, '\n');
     end

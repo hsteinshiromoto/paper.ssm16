cf772d9417ec51cfd1c521c5f308f16e48239b38
humberto.shiromoto <humberto.shiromoto@ieee.org>
2016-09-07 14:29:00 +1000

    add branch to the header

--- WriterW.m
+++ WriterW.m
@@ -1,6 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                         %
 % Title: Writer W                                                         %
+% Git Branch: Master                                                      %
 % Description: Creates the matrices W and DW for the LMI optmization      %
 % Inputs: N. Integer, Scalar, Number of Agents                            %
 %         SystemDimension. Integer, Scalar, Dimension of Each Subsystem   %

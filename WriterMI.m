c4738abfeccb400881ab8ba8f25e5022b7c68f0f
humberto.shiromoto <humberto.shiromoto@ieee.org>
2016-09-07 11:35:17 +1000

    Add a condition for solvibility of systems with more than 2 agents.

--- WriterMI.m
+++ WriterMI.m
@@ -27,34 +27,34 @@ NumberOfStates = length(SystemStates);
 
 fprintf(fid, 'MI = -DW + A*W + transpose(A*W) - R*B*transpose(B) + 2*%f*W;\n\n',lambda);
 
-
-for AgentsCounter = 1:NumberOfAgents - 1
-    
-    fprintf(fid, 'Block%s = ',num2str(AgentsCounter));
-    
-    if AgentsCounter == 1
-        fprintf(fid, 'MI(1:6,1:6);\n');
+if NumberOfAgents > 2
+    for AgentsCounter = 1:NumberOfAgents - 1
         
-    else
-        fprintf(fid, 'MI(4:9,4:9);\n');
+        fprintf(fid, 'Block%s = ',num2str(AgentsCounter));
         
+        if AgentsCounter == 1
+            fprintf(fid, 'MI(1:6,1:6);\n');
+            
+        else
+            fprintf(fid, 'MI(4:9,4:9);\n');
+            
+        end
+        fprintf(fid, 'Block%s(3:end,3:end) = Block%s(3:end,3:end)/2;\n',num2str(AgentsCounter),num2str(AgentsCounter));
+        
+        fprintf(fid, '\n');
     end
-    fprintf(fid, 'Block%s(3:end,3:end) = Block%s(3:end,3:end)/2;\n',num2str(AgentsCounter),num2str(AgentsCounter));
     
-    fprintf(fid, '\n');
-end
-
-fprintf(fid, 'MIConstraints = [');
-if NumberOfAgents > 1
+    fprintf(fid, 'MIConstraints = [');
     for AgentsCounter = 1:NumberOfAgents-1
         fprintf(fid, 'sos(-Block%s + %f*eye(size(Block%s)));',num2str(AgentsCounter),ScalingFactor,num2str(AgentsCounter));
     end
 else
+    fprintf(fid, 'MIConstraints = [');
     fprintf(fid, 'sos(-MI + %f*eye(size(MI)));',ScalingFactor);
 end
-    
-    
-    fprintf(fid, '];');
+
+
+fprintf(fid, '];');
     
 
 

c02b2b3b16a692df134e00df441f4afcd614b224
humberto.shiromoto <humberto.shiromoto@ieee.org>
2016-09-09 11:53:12 +1000

    Change Rho to have all variables.

--- WriterRho.m
+++ WriterRho.m
@@ -50,8 +50,7 @@ fprintf(fid, '%% NumberOfAgents = %d,\n%% NumberOfStates = %d,\n',NumberOfAgents
                 fprintf(fid, ' = polynomial([');
                 
                 for i=1:length(NonZeroIndexes)
-                  fprintf(fid, '[x%s;y%s];',num2str(NonZeroIndexes(i)));
-%                     fprintf(fid, 'q%s;',num2str(NonZeroIndexes(i)));
+                  fprintf(fid, 'q%s;',num2str(NonZeroIndexes(i)));
                 end
                 fprintf(fid, '],%d,0);\n',Rhodegree);
                 

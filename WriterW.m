63a97f17f616317163c90b40f099c63428452a99
humberto.shiromoto <humberto.shiromoto@ieee.org>
2016-09-09 11:47:56 +1000

    Nonlinear interconnection is working.

--- WriterW.m
+++ WriterW.m
@@ -49,8 +49,8 @@ for AgentsCounter = 1:NumberOfAgents
                 fprintf(fid, 'Wc%s%s%s,', num2str(AgentsCounter),num2str(RowCounter),num2str(ColumnCounter));
                 fprintf(fid, 'Wv%s%s%s]', num2str(AgentsCounter),num2str(RowCounter),num2str(ColumnCounter));
                 fprintf(fid, ' = polynomial(');
-%                 fprintf(fid, '[x%s;y%s],', num2str(AgentsCounter),num2str(AgentsCounter));
-                fprintf(fid, 'q%s,', num2str(AgentsCounter));
+                fprintf(fid, '[x%s;y%s],', num2str(AgentsCounter),num2str(AgentsCounter));
+%                 fprintf(fid, 'q%s,', num2str(AgentsCounter));
                 fprintf(fid, '%d,0);\n',Wdegree);
             
             end
